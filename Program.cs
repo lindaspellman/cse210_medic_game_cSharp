64b9ea6aabd3d80eca5356cc39717d645e7c2a34
Linda Spellman <lindaspellman@live.com>
2021-11-23 19:04:14 -0700

    added new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cse210_medic_game_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
