288aaa57ccd7c6916540f8d9fa16532682cff405
Linda Spellman <lindaspellman@live.com>
2021-12-08 11:13:07 -0700

    organized Constants class

--- Constants.cs
+++ Constants.cs
@@ -7,65 +7,53 @@ namespace cse210_medic_game_cSharp
     /// </summary>
     public static class Constants
     {
+        // POINTS 
         public const int VICTORY_POINTS = 300;
         public const int STARTING_POINTS = 50;
         public const int ENEMY_POINTS = -20;
         public const int CIVILIAN_POINTS = 15;
 
-
+        // DEFAULTS //////////////////////////////////////////////////////////////////////////
         public const int MAX_X = 800;
         public const int MAX_Y = 600;
         public const int FRAME_RATE = 30;
 
         public const int MINIMUM_SPEED = 4;
         public const int MAXIMUM_SPEED = 9;
-        public const int ENEMY_FRACTION = 5;
-
-
         public const int DEFAULT_SQUARE_SIZE = 20;
-        public const int DEFAULT_FONT_SIZE = 30;
 
+        public const int DEFAULT_FONT_SIZE = 30;
         public const int GAMEOVER_FONT_SIZE = 100;
         public const int DEFAULT_TEXT_OFFSET = 4;
 
-        // public const string IMAGE_BRICK = "./Assets/brick-3.png";
-        // public const string IMAGE_PADDLE = "./Assets/bat.png";
-        // public const string IMAGE_BALL = "./Assets/ball.png";
-
-        public const string SOUND_START = "./Assets/start.wav";
-        public const string SOUND_BOUNCE = "./Assets/boing.wav";
-        public const string SOUND_OVER = "./Assets/over.wav";
-
-        // public const int BALL_X = MAX_X / 2;
-        // public const int BALL_Y = MAX_Y - 125;
-
-        // public const int BALL_DX = 8;
-        // public const int BALL_DY = BALL_DX * -1;
-
-        public const int MEDIC_X = MAX_X / 2;
-        public const int MEDIC_Y = MAX_Y - 25;
-
+        // CIVILIAN //////////////////////////////////////////////////////////////////////////////////
         public const int CIVILIAN_WIDTH = 30;
         public const int CIVILIAN_HEIGHT = 30;
-
         public const int CIVILIAN_SPACE = 5;
-
         public const int CIVILIANS_PRESENT = 12;
 
+        // MEDIC //////////////////////////////////////////////////////////////////////////////////
+        public const int MEDIC_X = MAX_X / 2;
+        public const int MEDIC_Y = MAX_Y - 25;
         public const int MEDIC_SPEED = 15;
         public const int MEDIC_WIDTH = 25;
         public const int MEDIC_HEIGHT = 25;
 
+        // ENEMY ///////////////////////////////////////////////////////////////////////////////////
         public const int ENEMY_WIDTH = 25;
         public const int ENEMY_HEIGHT = 25;
-
         public const int ENEMY_SPACE = 5;
-
         public const int ENEMIES_PRESENT = 5;
+        public const int ENEMY_FRACTION = 5;
 
+        // ASSETS ////////////////////////////////////////////////////////////////////////////////////
+        public const string SOUND_START = "./Assets/start.wav";
+        public const string SOUND_BOUNCE = "./Assets/boing.wav";
+        public const string SOUND_OVER = "./Assets/over.wav"; 
+        // public const string IMAGE_BRICK = "./Assets/brick-3.png";
+        // public const string IMAGE_PADDLE = "./Assets/bat.png";
+        // public const string IMAGE_BALL = "./Assets/ball.png";
         
-        // public const int BALL_WIDTH = 24;
-        // public const int BALL_HEIGHT = 24;
     }
 
 }
